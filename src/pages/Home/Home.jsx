057d5ef72e3671fd03bc4e17bb6f27b3f45edd17
Bryan Claro Zimbrão <bryan.sup.shop@alterdata.matriz>
2025-04-10 09:22:01 -0300

    Manutenção links (API nova)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import 'bulma/css/bulma.min.css';
 import BtnInfo from '../components/BtnInfo';
 import BtnForm from '../components/BtnForm';
@@ -6,22 +7,20 @@ import BtnForm from '../components/BtnForm';
 const Home = () => {
   return (
     <>
-        <title>Loja de Eletrônicos</title>
+      <title>Loja de Eletrônicos</title>
 
-        <h1 id="topo">Tech Shop</h1>
-      
-          
-          <div id="box">
-              <a id="btn" href="/vitrine">
-                <img src="../IMG/Produtos.png" style={{ width: "100%" , height: "100%"}} alt="Produtos"/>
-              </a> 
-          </div>
-          
-         <BtnForm></BtnForm> 
-        <BtnInfo></BtnInfo>
+      <h1 id="topo">Tech Shop</h1>
 
+      <div id="box">
+        <Link id="btn" to="/vitrine">
+          <img src="../IMG/Produtos.png" style={{ width: "100%" , height: "100%"}} alt="Produtos"/>
+        </Link> 
+      </div>
+
+      <BtnForm />
+      <BtnInfo />
     </>
-  )
+  );
 };
 
 export default Home;
